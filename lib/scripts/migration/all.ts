d7c79f603486d05e6b37fa4960cfecfef242f170
Bebe Jane <bjornthief@gmail.com>
2023-05-31 15:16:34 +0200

    migr

---
+++ lib/scripts/migration/all.ts
@@ -0,0 +1,21 @@
+import { migrateAbout } from "./abouts.js";
+import { migrateArtists } from "./artists.js";
+import { migrateProjects } from "./projects.js";
+
+const migrateAll = async () => {
+  const subdomains = (await allDistricts()).map(d => d.subdomain)
+  console.time(`migrateall`)
+  for (let i = 0; i < subdomains.length; i++)
+    await migrate(subdomains[i])
+
+  console.timeEnd(`migrateall`)
+}
+
+export const migrate = async (subdomain?: string) => {
+  console.time(`migrate ${subdomain}`)
+  console.log(`\n-- ${subdomain} -----------------------------------`)
+  await Promise.allSettled([migrateAbout(subdomain), migrateNews(subdomain), migrateProjects(subdomain)]);
+  console.timeEnd(`migrate ${subdomain}`)
+}
+
+migrateAll()
